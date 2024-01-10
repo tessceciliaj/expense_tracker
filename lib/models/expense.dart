257883c99cc5ddff81dcb63c701b13df1a5fc81c
Therese Cecilia Johansson <Theresececilias@gmail.com>
2024-01-10 14:04:37 +0100

    Add a map for icons, intl for date formatting

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,10 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 //fixed set of values
 // enum is a keyword that allows us to create a costum type
 enum Category { food, travel, leisure, work }
+
+// Value is a map where the keys are the enum Category
+const categoryIcons = {
+  Category.food: Icons.lunch_dining_outlined,
+  Category.travel: Icons.flight_takeoff_outlined,
+  Category.leisure: Icons.movie_outlined,
+  Category.work: Icons.work
+};
 
 class Expense {
   Expense({
@@ -19,4 +31,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
