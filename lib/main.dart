db2e94152e37e3c591805b374993104ba6fa2bcb
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2024-01-11 10:48:28 +0100

    Create add expense modal in new_expense.dart, create controller to text field.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Expenses(),
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      home: const Expenses(),
+    );
+  }
 }
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  //create controller
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  //Dispose controller after use, flutter automatically handles this
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          TextField(
+            controller: _amountController,
+            maxLength: 50,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$',
+              label: Text('Amount'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text('Save expense'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(color: Color.fromARGB(255, 130, 130, 130)),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
