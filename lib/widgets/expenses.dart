974a50224b3f4a1cd8a0fa8b1d97ada6bdd6e0f0
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2024-01-11 15:00:31 +0100

    Remove expense card functionality

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -42,8 +42,25 @@ class _ExpensesState extends State<Expenses> {
     );
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registerExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registerExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registerExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepOrange[800],
@@ -59,7 +76,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('The Chart'),
-          Expanded(child: ExpensesList(expenses: _registerExpenses)),
+          Expanded(child: mainContent),
         ],
       ),
     );
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,15 +3,27 @@ import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
+  final void Function(Expense expense) onRemoveExpense;
   final List<Expense> expenses;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        onDismissed: (direction) {
+          onRemoveExpense(
+            expenses[index],
+          );
+        },
+        key: ValueKey(expenses[index]),
+        child: ExpenseItem(
+          expenses[index],
+        ),
+      ),
     );
   }
 }
